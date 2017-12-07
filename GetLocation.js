dfa218c298124742e906ef02f4afa346398e66a0
Yu Chun Tsao <tsao84672776@gmail.com>
2017-12-07 23:50:37 +0800

    Add files via upload

---
+++ GetLocation.js
@@ -0,0 +1,25 @@
+//網頁載入後執行。
+window.onload = main;
+
+function GetGeolcation(){
+	if(navigator.geolocation){
+		//watchPosition ==> 持續回傳用戶移動時的更新位置
+		navigator.geolocation.watchPosition(ShowPosition);
+	}
+	else{
+		alert("Geolocation is not supported by this browser.");
+	}
+}
+function ShowPosition(position){
+	var latlng = {
+		"latitude":position.coords.latitude,
+		"longitude":position.coords.longitude
+	}
+	document.write(JSON.stringify(latlng) + "<br>");
+	//console.log(JSON.stringify(latlng));
+}
+
+function main(){
+	//setInterval(GetGeolcation , 1000);
+	GetGeolcation();
+}
